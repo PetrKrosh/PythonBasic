d72965f520f5ec1402205a9aba060a423fb33a94
PetrKrosh <pkrosh@ya.ru>
2022-02-10 16:23:18 +0300

    Задания выполнены

--- M16/01_scary_code/main.py
+++ M16/01_scary_code/main.py
@@ -3,13 +3,28 @@ b = [1, 5, 1, 5]
 c = [1, 3, 1, 5, 3, 3]
 
 a.extend(b)
-print('Кол-во цифр 5 при первом объединении:', a.count(5))
+print('Количество цифр 5 равно ', a.count(5))
 for _ in a:
-    if 5 in a:
-        a.remove(5)
-    else:
-        continue
+	if 5 in a:
+		a.remove(5)
+	else:
+		continue
+print('Список после добавления списка b', a)
 
 a.extend(c)
-print('Кол-во цифр 3 при втором объединении:', a.count(3))
-print('Итоговый список:', a)
+print('Количество цифр 3 равно ', a.count(3))
+print('Итоговый список: ', a)
+
+
+#
+# a.extend(b)
+# print('Кол-во цифр 5 при первом объединении:', a.count(5))
+# for _ in a:
+#     if 5 in a:
+#         a.remove(5)
+#     else:
+#         continue
+#
+# a.extend(c)
+# print('Кол-во цифр 3 при втором объединении:', a.count(3))
+# print('Итоговый список:', a)
--- M16/02_line/main.py
+++ M16/02_line/main.py
@@ -1,15 +1,44 @@
 first_class = []
 second_class = []
+# Вариант 1. Как наполнить список
+# for i in range(160,178,2):
+#     first_class.append(i)
+# Вариант 2. Как наполнить список
+first_class = list(range(160, 178, 2))
+second_class = list(range(162, 183, 3))
 
-for i_grow1 in range(160, 177, 2):
-    first_class.append(i_grow1)
-
-for i_grow2 in range(162, 181, 3):
-    second_class.append(i_grow2)
+print(first_class)
+print(second_class)
 
 first_class.extend(second_class)
-for _ in range(len(first_class) - 1):
-    for sort in range(len(first_class) - 1):
-        if first_class[sort] > first_class[sort + 1]:
-            first_class[sort], first_class[sort + 1] = first_class[sort + 1], first_class[sort]
-print('Все ученики по росту: ', first_class)
+first_class.sort()
+
+print('Итоговый список:', first_class)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# first_class = []
+# second_class = []
+#
+# for i_grow1 in range(160, 177, 2):
+#     first_class.append(i_grow1)
+#
+# for i_grow2 in range(162, 181, 3):
+#     second_class.append(i_grow2)
+#
+# first_class.extend(second_class)
+# for _ in range(len(first_class) - 1):
+#     for sort in range(len(first_class) - 1):
+#         if first_class[sort] > first_class[sort + 1]:
+#             first_class[sort], first_class[sort + 1] = first_class[sort + 1], first_class[sort]
+# print('Все ученики по росту: ', first_class)
